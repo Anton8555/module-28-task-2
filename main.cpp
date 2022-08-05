89e771915855b31532a9d73f9323a44bd8445caa
Anton Ugryumov <anton.ugriumov@yandex.ru>
2022-08-05 14:33:23 +0300

    initial-4

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <queue>
 #include <thread>
 #include <mutex>
 using namespace std;
@@ -9,9 +8,15 @@ using namespace std;
 
 // data
 
-// common data for threads
-queue<string> railwayStation;
+// data for threads
+string railwayStation;
 mutex railwayStation_access;
+
+// stopping point number 1
+mutex stopPoint_1;
+
+// stopping point number 2
+mutex stopPoint_2;
 
 // condition variable: message output queue to the console
 mutex fOut_access;
@@ -35,10 +40,26 @@ void thread_train(const string& label, int time) {
     cout << "Train '" << label << "' is waiting for a free seat at the station.\n";
     fOut_access.unlock();
 
-    // queuing a train
+    // we are trying to pass the breakpoint number 2 - we are waiting until the already occupied space is freed up.
+    stopPoint_2.lock();
+
+    // write down the designation of the train arriving at the station
     railwayStation_access.lock();
-    railwayStation.push(label);
+    railwayStation = label;
     railwayStation_access.unlock();
+
+    // message output
+    fOut_access.lock();
+    cout << "Train '" << label << "' arrived at the station.\n";
+    fOut_access.unlock();
+
+    // trying to pass breakpoint #1 - waiting for the 'depart' command to execute.
+    stopPoint_1.lock();
+
+    // conclusion about the departure of the train
+    fOut_access.lock();
+    cout << "Train '" << label << "' has departed.\n";
+    fOut_access.unlock();
 }
 
 
@@ -52,10 +73,14 @@ int main() {
         string label;             // train symbol
         int time = 0;             // train travel time
         thread* track = nullptr;  // thread object
-        bool flag = false;        // train arrival flag
     } train[N];
     // flag
     bool everyoneWasAtTheStation;
+    // train label
+    string trainLabel;
+
+
+
 
     // data input
     for(auto &t: train) {
@@ -66,38 +91,29 @@ int main() {
     }
     cout << endl;
 
+    // preparation of the station for the arrival of the train
+    stopPoint_1.lock();
+    railwayStation = "";
+
     // start threads
     for(auto& t: train)
         t.track = new thread(thread_train, t.label, t.time);
 
     // station work
     do{
-        // data
-        string trainLabel;
-
         // pause (poll the queue twice per second)
         this_thread::sleep_for(chrono::milliseconds(500));
 
         // get value from queue
         railwayStation_access.lock();
-        if( !railwayStation.empty() ) {
-            trainLabel = railwayStation.front();
-            railwayStation.pop();
-        } else {
-            trainLabel = "none";
-        }
+        trainLabel = railwayStation;
         railwayStation_access.unlock();
 
         // if it is a train designation
-        if( trainLabel != "none" ) {
+        if( trainLabel.length() != 0 ) {
             // data
             string command;
             bool commandCompletedFlag;
-
-            // then the conclusion about the arrival of the train at the station
-            fOut_access.lock();
-            cout << "Train '" << trainLabel << "' arrived at the station.\n";
-            fOut_access.unlock();
 
             // command input and execution cycle
             commandCompletedFlag = false;  // there is no executed command yet
@@ -110,17 +126,24 @@ int main() {
 
                 // command analysis
                 if (command == "depart") {
-                    // conclusion about the departure of the train
-                    fOut_access.lock();
-                    cout << "Train '" << trainLabel << "' has departed.\n";
-                    fOut_access.unlock();
-
                     // departed train mark
+                    // unblocking a breakpoint to send a train
+                    stopPoint_1.unlock();
+                    // waiting for the thread to complete
                     for (auto &t: train)
                         if (trainLabel == t.label) {
-                            t.flag = true;
+                            t.track->join();
+                            t.track = nullptr;
                             break;
                         }
+
+                    // when the train is sent, we make preparations for the arrival of the next train
+                    // clearing the train marker
+                    railwayStation_access.lock();
+                    railwayStation = "";
+                    railwayStation_access.unlock();
+                    // and unblock breakpoint #2 to allow the arrival of the next train
+                    stopPoint_2.unlock();
 
                     // flag setting - the command is executed.
                     commandCompletedFlag = true;
@@ -131,8 +154,12 @@ int main() {
         // if all trains have been at the station, then exit the station maintenance cycle
         everyoneWasAtTheStation = true;
         for(auto &t: train)
-            everyoneWasAtTheStation &= t.flag;
+            everyoneWasAtTheStation &= (t.track == nullptr);
     }while( !everyoneWasAtTheStation );
+
+    // data reset
+    stopPoint_1.unlock();
+    railwayStation = "";
 
     return 0;
 }
